5a787f3f2b4d45e6ef7dda6119d6076d2a7ac9d2
mohammad-talib <mohammad.h.alhety@gmail.com>
2019-07-10 12:22:56 +0300

    quiz

---
+++ exercises/js quiz/quiz4.js
@@ -0,0 +1,14 @@
+
+/*
+ * Programming Quiz: First Expression 
+ *
+ * Write an expression that uses at least three, different, arithmetic operators
+ * to log the number 42 to the console.
+ */
+
+// this expression equals 4, change it to equal 42
+
+console.log(6*4+8+10);
+console.log(8*8/2+10);
+console.log(20*2+2);
+console.log(6*6+6);
---
+++ exercises/js quiz/quiz5.js
@@ -0,0 +1,6 @@
+/*
+ * Programming Quiz: Favorite Food 
+ * Create a string with the name of your favorite food. The first letter of the string should be capitalized.
+ */
+var favoriteFood = "Pizza"
+console.log(favoriteFood);
